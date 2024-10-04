b680e4315d8989c3f45f50fd5e02cb594a0a0c60
eazzie960904 <eazzie960904@gmail.com>
2024-10-04 13:21:29 +0900

    name redeploy

--- src/components/KakaoShareButton.jsx
+++ src/components/KakaoShareButton.jsx
@@ -17,7 +17,7 @@ const KakaoShareButton = ({ data }) => {
       objectType: "feed",
       content: {
         title: "예비집사 판별기 결과",
-        description: `예비집사님이 고양이를 키운다면 가장 잘 맞는 고양이는 {data.naem}입니다.`,
+        description: `예비집사님이 고양이를 키운다면 가장 잘 맞는 고양이는 ${data.name}입니다.`,
         imageUrl: `${url}${data.image}`,
         link: {
           mobileWebUrl: resultURL,
